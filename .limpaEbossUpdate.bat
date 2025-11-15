95ca3f658327e0ad8f57373ca870228a31f75b4f
Anderson Rogerio <44527776+arbsis@users.noreply.github.com>
2025-11-15 01:10:59 -0300

    Evoluções Boss Anderson

---
+++ .limpaEbossUpdate.bat
@@ -0,0 +1,19 @@
+del *.dcu
+del *.~??
+del *.ddp
+del *.drc
+del *.stat
+del *.dsk
+rd /s /q modules
+rd /s /q Win32
+rd /s /q Win64
+rd /s /q C:\Users\Ander\.boss\cache
+rd /s /q C:\Users\Ander\.boss\env
+rd /s /q C:\Users\Adriano\.boss\cache
+rd /s /q C:\Users\Adriano\.boss\env
+rd /s /q C:\Users\music\.boss\cache
+rd /s /q C:\Users\music\.boss\env
+rd /s /q C:\Users\Programacao2\.boss\cache
+boss update
+pause
+exit
